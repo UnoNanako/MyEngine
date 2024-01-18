860e6408c101c33e6a9bc2441f06b9947d60d7f1
MSI\GSManager <unonnk@g.neec.ac.jp>
2024-01-18 15:32:36 +0900

    HalfLambert実装完了

--- resources/shaders/Object3d.PS.hlsl
+++ resources/shaders/Object3d.PS.hlsl
@@ -18,7 +18,11 @@ PixelShaderOutput main(VertexShaderOutput input) {
     PixelShaderOutput output;
     float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     if(gMaterial.enableLighting != 0){
-        float cos = saturate(dot(normalize(input.normal),-gDirectionalLight.direction));
+        //HalfLambert
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+        //Lambert
+        //float cos = saturate(dot(normalize(input.normal),-gDirectionalLight.direction));
         output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
     }else{ //Lightingしない場合。前回までと同じ演算
         output.color = gMaterial.color * textureColor;
